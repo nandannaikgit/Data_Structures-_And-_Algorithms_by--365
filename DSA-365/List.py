80973af8508c23e97c4a9e74451f49c61caa7750
nandannaikgit <nandannaik415@gmail.com>
2025-10-17 07:14:39 +0530

    17/10/2025

--- DSA-365/List.py
+++ DSA-365/List.py
@@ -1,16 +1,40 @@
 # Day 2  --> List 
 
-numbers = [1,2,3,4]
+# numbers = [1,2,3,4]
 
-print(numbers[0])
-print(numbers[1])
-print(numbers[2])
-print(numbers[3])
+# print(numbers[0])
+# print(numbers[1])
+# print(numbers[2])
+# print(numbers[3])
 
-print(numbers)
+# print(numbers)
 
-numbers[0] = 100
-print(numbers)
+# numbers[0] = 100
+# print(numbers)
 
-numbers[1] = "a"
-print(numbers)
+# numbers[1] = "a"
+# print(numbers)
+
+
+num = [1,2,3]
+
+num.append(5)
+num.append(4)
+print(num)
+
+num.sort()
+print(num)
+
+num.insert(2,100)
+print(num)
+
+num.remove(5)
+print(num)
+
+num1 = [0,9,8,7]
+num.extend(num1)
+print(num)
+
+# num.reverse()
+print(num)
+

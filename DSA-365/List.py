e9e8bc8a9969d585b7ce4b3377489d31071d899c
nandannaikgit <nandannaik415@gmail.com>
2025-10-17 07:01:11 +0530

    17/10/2025

---
+++ DSA-365/List.py
@@ -0,0 +1,16 @@
+# Day 2  --> List 
+
+numbers = [1,2,3,4]
+
+print(numbers[0])
+print(numbers[1])
+print(numbers[2])
+print(numbers[3])
+
+print(numbers)
+
+numbers[0] = 100
+print(numbers)
+
+numbers[1] = "a"
+print(numbers)
